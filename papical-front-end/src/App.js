3fcc364e2a56c55708c681240bdca151483b812a
cindychea <cindy_chea@hotmail.com>
2019-07-12 17:26:43 -0400

    Added log in page, minor styling, working on axios request to back end.

--- papical-front-end/src/App.js
+++ papical-front-end/src/App.js
@@ -7,6 +7,10 @@ import Friends from "./Friends.js";
 import Notifications from "./Notifications.js";
 import User from "./User.js";
 import SignIn from "./SignIn.js";
+import LogIn from "./LogIn.js";
+import SignUp from "./SignUp.js";
+import './reset.css';
+import './normalize.css';
 import './App.css';
 
 require('dotenv').config()
@@ -14,9 +18,11 @@ require('dotenv').config()
 function AppRouter() {
 
   const [state, setState] = useState({
+    isInsider: false,
     isLoggedIn: false,
   });
 
+  
   function onLogInFunc({username, password}) {
     
     const urlToken = "http://localhost:8000/login/";
@@ -24,51 +30,98 @@ function AppRouter() {
     axios.post(urlToken, {
       username,
       password,
-      })
-      .then(function(response) {
-        console.log(response.data.access)
-        localStorage.setItem('accesstoken', response.data.access)
-        localStorage.setItem('refreshtoken', response.data.refresh)
-        setState({
-          isLoggedIn: true,
-        });
-      })
+    })
+    .then(function(response) {
+      console.log(response.data.access)
+      localStorage.setItem('accesstoken', response.data.access)
+      localStorage.setItem('refreshtoken', response.data.refresh)
+      setState({
+        isInsider: true,
+        isLoggedIn: true,
+      });
+    })
       .catch(function (error) {
         console.log(error);
       });
-  }
-
-  return (
+    }
+    
+    function onSignUpFunc() {
+      setState({
+        isInsider: true,
+      });
+    }
+    
+    function getActiveNav() {
+      switch (state.isInsider && state.isLoggedIn) {
+        case false:
+          return outerNav();
+        case true:
+          return innerNav();
+        default:
+          return outerNav();
+      }
+    }
+  
+    function outerNav() {
+      return (
+        <React.Fragment>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/login">Log In</Link>
+              </li>
+              <li>
+                <Link to="/signup">Sign Up</Link>
+              </li>
+            </ul>
+          </nav>
+        </React.Fragment>
+      )
+    }
+  
+    function innerNav() {
+      return (
+        <React.Fragment>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Dashboard</Link>
+              </li>
+              <li>
+                <Link to="/calendar">Calendar</Link>
+              </li>
+              <li>
+                <Link to="/friends">Friends</Link>
+              </li>
+              <li>
+                <Link to="/notifications">Notifications</Link>
+              </li>
+              <li>
+                <Link to="/user">User</Link>
+              </li>
+            </ul>
+          </nav>
+        </React.Fragment>
+      )
+    }
+  
+    return (
     <BrowserRouter>
       <div className="App">
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Dashboard</Link>
-            </li>
-            <li>
-              <Link to="/calendar">Calendar</Link>
-            </li>
-            <li>
-              <Link to="/friends">Friends</Link>
-            </li>
-            <li>
-              <Link to="/notifications">Notifications</Link>
-            </li>
-            <li>
-              <Link to="/user">User</Link>
-            </li>
-          </ul>
-        </nav>
-
+        {getActiveNav()}
         <h1>Papical</h1>
         <Route path="/" exact component={Dashboard} />
         <Route path="/calendar/" component={Calendar} />
         <Route path="/friends/" component={Friends} />
         <Route path="/notifications/" component={Notifications} />
         <Route path="/user/" component={User} />
-        {state.isLoggedIn ? '' : <SignIn onLogInFunc={onLogInFunc}></SignIn>}
-        
+        <Route path="/signup/" component={SignUp} />
+        <Route path="/login/"
+               render={(routeProps) => (
+                 <LogIn {...routeProps} onLogInFunc={onLogInFunc} />
+               )} 
+         />
+        {state.isInsider ? '' : <SignIn onLogInFunc={onLogInFunc} onSignUpFunc={onSignUpFunc} />}
       </div>
     </BrowserRouter>
   );
--- papical-front-end/src/SignIn.js
+++ papical-front-end/src/SignIn.js
@@ -1,18 +1,31 @@
 import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import Icon from './Icon.png'
 
-function SignIn({onLogInFunc}) {
+function SignIn({onLogInFunc, onSignUpFunc}) {
 
   const [state, setState] = useState({
     username: '',
     password: '',
   });
 
-  return <div className="sign-in-page">
-      <input type='text' name='Username' placeholder='Username' onChange={(e) => setState({...state, username: e.target.value})}/>
-      <input type='password' name='Password' placeholder='Password'onChange={(e) => setState({...state, password: e.target.value})}/>
-      <button onClick={() => onLogInFunc(state)}>Log In</button>
-      <button>Sign Up</button>
+  return (
+  <div className="sign-in-page">
+    <div className="sign-in">
+      <h1 className="sign-in-title">Papical</h1>
+      <h4>An App that makes planning hangouts with friends simple.</h4>
+      Username <input type='text' name='Username' placeholder='Username' onChange={(e) => setState({...state, username: e.target.value})}/>
+      Password <input type='password' name='Password' placeholder='Password'onChange={(e) => setState({...state, password: e.target.value})}/>
+      <br />
+      <NavLink to="/" onClick={() => onLogInFunc(state)}>Log In</NavLink>
+      <p>or</p>
+      <NavLink to="/signup" onClick={onSignUpFunc}>Sign Up</NavLink>
+    </div>
+    <div className="logo">
+      <img className="papical-logo" src={Icon} alt="Papical Logo"/>
+    </div>
   </div>
-}
+  )
+};
 
 export default SignIn;
