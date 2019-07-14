361b516ce4a667a60b6837b0720095a2930fae90
Jasmin Saromo <jasminsaromo@gmail.com>
2019-07-13 21:04:44 -0400

    Created unique list of friends from to_user and from_user.

--- papical-front-end/src/Friends.js
+++ papical-front-end/src/Friends.js
@@ -4,15 +4,37 @@ import axios from 'axios';
 // import { userInfo } from 'os';
 
 function Friends() {
-
+  
   const [friends, setFriends] = useState([])
   const [currentUser, setCurrentUser] = useState([])
   
+  // getting current information
+  useEffect( () => {
+  
+    const url = 'http://localhost:8000/users/'
+    axios.get(url, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('accesstoken')}`
+      } 
+    }).then(function (response) {
+        // handle success
+        setCurrentUser(response.data)
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+  
+  }, [])
 
+  // getting unique list of friendships
   useEffect( () => {
 
       const url = 'http://localhost:8000/friends/'
-      axios.get(url, {headers: {Authorization: `Bearer ${localStorage.getItem('accesstoken')}`} 
+      axios.get(url, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('accesstoken')}`
+        } 
       }).then(function (response) {
           // handle success
           // console.log('data', response.data);
@@ -25,36 +47,51 @@ function Friends() {
 
     }, [])
 
-  useEffect( () => {
-
-    const url = 'http://localhost:8000/users/'
-    axios.get(url, {headers: {Authorization: `Bearer ${localStorage.getItem('accesstoken')}`} 
-    }).then(function (response) {
-        // handle success
-        // console.log('data', response.data);
-        setCurrentUser(response.data)
-      })
-      .catch(function (error) {
-        // handle error
-        console.log(error);
-      })
-
-  }, [])
-
+  //getting friends list
+  let friendUsers = []
   const FriendsList = (friend) => {
-    let user = currentUser[0];
+    const user = currentUser[0];
+    const from_user = friend.from_user
+    const to_user = friend.to_user
 
-    if (friend.from_user.pk !== user.pk) {
- 
-      return (
-          <li key={friend.pk}>
-            <p>{friend.from_user.picture}</p>
-            <p>{friend.from_user.username}</p>
-            <p>{friend.from_user.first_name} {friend.from_user.last_name}</p>
-          </li>
-        )
+    if (to_user.pk !== user.pk) {
+      friendUsers.push(to_user)
+    }
+    if (from_user.pk !== user.pk) {
+      friendUsers.push(from_user)
+    }
+
+    //getting unique friends
+    var listOfFriends = friendUsers.reduce((unique, o) => {
+      if(!unique.some(obj => obj.pk === o.pk)) {
+        unique.push(o);
       }
+      return unique;
+    },[])
+
+    console.log(listOfFriends)
+
+    // listOfFriends.forEach((user) => {
+    //   console.log(user);
+
+    // })
+
+
+    // return (
+    //     <li key={friend.pk}>
+    //       <p>{to_user.picture}</p>
+    //       <p>{to_user.username}</p>
+    //       <p>{to_user.first_name} {to_user.last_name}</p>
+    //     </li>
+    //   )
+    
+
   }
+
+
+
+
+
 
   return (
     <section>
