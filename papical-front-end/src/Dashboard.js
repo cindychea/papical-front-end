a94bd1a159cad4f48965eb37ee030b795c2669cc
cindychea <cindy_chea@hotmail.com>
2019-07-14 17:46:24 -0400

    Started creating dashboard page.

--- papical-front-end/src/Dashboard.js
+++ papical-front-end/src/Dashboard.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { request } from 'https';
 
 function Dashboard() {
-  return <h2>Dashboard</h2>;
+  return (
+    <div className="dashboard">
+      <h2 className="sign-up-header">Hi ~ user's first name here ~</h2>
+      <div className="activity">
+        <p>You have</p>
+        {/* make these into links! */}
+        <p>~number~ upcoming hangouts this week</p>
+        <p>~number~ pending invitations</p>
+      </div>
+      <div>
+        <p>Your weekly summary:</p>
+        <button>Book a hangout</button>
+        {/* add calendar */}
+      </div>
+    </div>
+  )
 }
 
 export default Dashboard;
