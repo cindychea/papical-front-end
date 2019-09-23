91e3fdfa8cdc78d29b228ae60631c9f74df22ca1
Jasmin Saromo <jasminsaromo@gmail.com>
2019-09-23 13:52:05 -0400

    search for users on friends page

--- papical-front-end/src/Friends.js
+++ papical-front-end/src/Friends.js
@@ -8,12 +8,12 @@ function Friends() {
 
   const [friendships, setFriendships] = useState([]);
   const [currentUser, setCurrentUser] = useState({});
+  const [listOfUsers, setListOfUsers] = useState([]);
+  const [results, setResults] = useState([]);
   const [state, setState] = useState({
     query: '',
-    results: []
   });
 
-  
 // Getting current user information
   useEffect( () => {
   
@@ -51,43 +51,77 @@ function Friends() {
       })
   }, [])
 
+  // GETTING LIST OF USERS
+  useEffect( () => {
+  
+    const url = 'http://localhost:8000/users/'
+    axios.get(url, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('accesstoken')}`
+      } 
+    }).then(function (response) {
+        // handle success
+        setListOfUsers(response.data)
+      })
+      .catch(function (error) {
+        const refreshUrl = 'http://localhost:8000/refresh/'
+        if (error.response.data.code === 'token_not_valid') {
+          axios.post(refreshUrl, {refresh: localStorage.getItem('refreshtoken')})
+          .then(function (response) {
+            localStorage.setItem('accesstoken', response.data.access)
+          })
+          .then(function(response) {
+            const url = 'http://localhost:8000/users/'
+            axios.get(url, {
+              headers: {
+                Authorization: `Bearer ${localStorage.getItem('accesstoken')}`
+              } 
+            })
+          })
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+  }, [])
+  
   // Getting list of friendships
   useEffect( () => {
 
-      const url = 'http://localhost:8000/friends/'
-      axios.get(url, {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem('accesstoken')}`
-        } 
-      }).then(function (response) {
-          // handle success
-          setFriendships(response.data)
-        })
-        .catch(function (error) {
-          // handle error
-          // console.log(error);
-          // console.log(error.response.data.code);
-          const refreshUrl = 'http://localhost:8000/refresh/'
-          if (error.response.data.code === 'token_not_valid') {
-            axios.post(refreshUrl, {refresh: localStorage.getItem('refreshtoken')})
-            .then(function (response) {
-              localStorage.setItem('accesstoken', response.data.access)
+    const url = 'http://localhost:8000/friends/'
+    axios.get(url, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('accesstoken')}`
+      } 
+    }).then(function (response) {
+        // handle success
+        setFriendships(response.data)
+      })
+      .catch(function (error) {
+        // handle error
+        // console.log(error);
+        // console.log(error.response.data.code);
+        const refreshUrl = 'http://localhost:8000/refresh/'
+        if (error.response.data.code === 'token_not_valid') {
+          axios.post(refreshUrl, {refresh: localStorage.getItem('refreshtoken')})
+          .then(function (response) {
+            localStorage.setItem('accesstoken', response.data.access)
+          })
+          .then(function(response) {
+            const url = 'http://localhost:8000/friends/'
+            axios.get(url, {
+              headers: {
+                Authorization: `Bearer ${localStorage.getItem('accesstoken')}`
+              } 
+            }).then(function (response) {
+              setFriendships(response.data)
             })
-            .then(function(response) {
-              const url = 'http://localhost:8000/friends/'
-              axios.get(url, {
-                headers: {
-                  Authorization: `Bearer ${localStorage.getItem('accesstoken')}`
-                } 
-              }).then(function (response) {
-                setFriendships(response.data)
-              })
-            })
-          }
-        })
-        .catch(function (error) {
-          console.log(error);
-        })
+          })
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
   }, [])
 
   const FriendDetails = (friendship) => {
@@ -105,25 +139,64 @@ function Friends() {
     
     const imageUrl = (friend.picture === null) ? `http://localhost:8000/media/images/profile_icon.svg` : `http://localhost:8000/${friend.picture}`
     
+    if (state.query.length < 1) {
 
-    return (
-      <Link id="notif-friend" to={{
-        pathname: "/friendprofile",
-        friendProfileProps:{
-          friend: friend
-        }
+      return (
+        <Link id="notif-friend" to={{
+          pathname: "/friendprofile",
+          friendProfileProps:{
+            friend: friend
+          }
         }} key={friend.pk} className="friend">
         <img src={`${imageUrl}`} alt={friend.username} className="friends-photo"/>
         <p className="friend-name">{friend.first_name} {friend.last_name}</p>
-      </Link>
-    )
+        </Link>
+      )
+    }
+  }
+
+  const getResults = () => {
+    const users = listOfUsers.filter(user => 
+      user.username.toLowerCase().includes(state.query) || 
+      user.first_name.toLowerCase().includes(state.query) ||
+      user.last_name.toLowerCase().includes(state.query) ||
+      user.email.toLowerCase().includes(state.query)
+      )
+    setResults(users)
+  }
+  
+  const Suggestions = (result) => {
+    const user = result
+
+    const imageUrl = (user.picture === null) ? `http://localhost:8000/media/images/profile_icon.svg` : `http://localhost:8000/${user.picture}`
+
+    if (state.query.length > 1) {
+
+      return (
+        <Link id="notif-friend" to={{
+          pathname: "/friendprofile",
+          friendProfileProps:{
+            friend: user
+          }
+        }} key={user.pk} className="friend">
+        <img src={`${imageUrl}`} alt={user.username} className="friends-photo"/>
+        <p className="friend-name">{user.first_name} {user.last_name}</p>
+        </Link>
+      )
+    }
   }
 
   const handleInputChange = () => {
     setState({
       query: searchBar.value
     })
-  }
+    if (state.query && state.query.length > 1) {
+      if (state.query.length % 2 === 0) {
+        getResults()
+      }
+    } else if (!state.query) {
+      }
+    }
 
   return (
     <section className="friends-box">
@@ -136,12 +209,11 @@ function Friends() {
             type='text'
             name='friends'
             placeholder='Search by username/name/email'
-            // ref={searchBar}
             onChange={handleInputChange}
           />
           <button className="std-btn base">Add friends</button>
         </div>
-        <p>{state.query}</p>
+        {results.map((result) => Suggestions(result))}
         {friendships.map((friendship) => FriendDetails(friendship))}
       </div>
     </section>
